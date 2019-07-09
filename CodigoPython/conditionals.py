1ce6fcbe5113ee681e69836a75e24eb44b10cc66
JoyceLA <joycseleneanaid@gmail.com>
2019-07-09 11:34:08 -0500

    Condicionales en colecciones

--- CodigoPython/conditionals.py
+++ CodigoPython/conditionals.py
@@ -19,3 +19,14 @@ else:
 if (x > 2):
 	if (x <= 10):
 		print(f'{x} es mas grande que 2 y menos o igual que 10')
+
+
+numbers = [1,2,3,4,5]
+z = 5
+
+if z in numbers:
+	print(f'{z} esta en la coleccion {numbers}')
+
+y = 10
+if y not in numbers:
+	print(f'{z} no esta en la coleccion {numbers}')
